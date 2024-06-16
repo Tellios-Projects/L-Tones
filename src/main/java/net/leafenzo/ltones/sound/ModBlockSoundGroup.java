d6f962ac1e7125d18bd34a22bd99b8ff31ee3b7e
Leafenzo <leafenzo@gmail.com>
2024-06-16 00:04:05 -0600

    Assign Decal BlockSoundGroups

--- src/main/java/net/leafenzo/ltones/sound/ModBlockSoundGroup.java
+++ src/main/java/net/leafenzo/ltones/sound/ModBlockSoundGroup.java
@@ -15,4 +15,6 @@ public class ModBlockSoundGroup {
     public static final BlockSoundGroup LAIR = new BlockSoundGroup(1.0f, 1.0f, ModSoundEvents.BLOCK_LAIR_BREAK, ModSoundEvents.BLOCK_LAIR_STEP, ModSoundEvents.BLOCK_LAIR_PLACE, ModSoundEvents.BLOCK_LAIR_HIT, ModSoundEvents.BLOCK_LAIR_FALL);
     public static final BlockSoundGroup SOL = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_GLASS_BREAK, SoundEvents.BLOCK_GLASS_STEP, SoundEvents.BLOCK_SHROOMLIGHT_PLACE, SoundEvents.BLOCK_GLASS_HIT, SoundEvents.BLOCK_GLASS_FALL);
     public static final BlockSoundGroup ZKUL = new BlockSoundGroup(1.0f, 1.0f, ModSoundEvents.BLOCK_ZKUL_BREAK, ModSoundEvents.BLOCK_ZKUL_STEP, ModSoundEvents.BLOCK_ZKUL_PLACE, ModSoundEvents.BLOCK_ZKUL_HIT, ModSoundEvents.BLOCK_ZKUL_FALL);
+    public static final BlockSoundGroup DECAL_SLAG = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_STONE_HIT, SoundEvents.BLOCK_STONE_STEP, SoundEvents.BLOCK_STONE_HIT, SoundEvents.BLOCK_STONE_HIT, SoundEvents.BLOCK_STONE_FALL);
+    public static final BlockSoundGroup DECAL_MOULDING = new BlockSoundGroup(0.33f, 1.0f, SoundEvents.BLOCK_GRAVEL_BREAK, SoundEvents.BLOCK_SUSPICIOUS_GRAVEL_STEP, SoundEvents.BLOCK_SUSPICIOUS_GRAVEL_PLACE, SoundEvents.BLOCK_GRAVEL_HIT, SoundEvents.BLOCK_SUSPICIOUS_GRAVEL_FALL);
 }
