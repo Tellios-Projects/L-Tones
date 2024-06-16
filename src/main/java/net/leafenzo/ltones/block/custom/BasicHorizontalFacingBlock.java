fad4e7b17d23a7ee7b332db89c0379ccae77bea7
Leafenzo <leafenzo@gmail.com>
2024-06-16 13:06:15 -0600

    Create BasicHorizontalFacingBlock.java

---
+++ src/main/java/net/leafenzo/ltones/block/custom/BasicHorizontalFacingBlock.java
@@ -0,0 +1,36 @@
+package net.leafenzo.ltones.block.custom;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class BasicHorizontalFacingBlock extends HorizontalFacingBlock {
+
+    public BasicHorizontalFacingBlock(AbstractBlock.Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return super.getPlacementState(ctx).with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(HorizontalFacingBlock.FACING);
+    }
+}
