b22068b75ee082aa68e0800b7b25405faabc359c
amber-meow <maplebernosky@gmail.com>
2025-03-13 10:30:19 -0400

    simplify gormless radio model

--- src/main/java/net/leafenzo/ltones/block/ModBlocks.java
+++ src/main/java/net/leafenzo/ltones/block/ModBlocks.java
@@ -802,6 +802,7 @@ public class ModBlocks {
     public static final Block ANTENNA = registerBlock("antenna", new AntennaBlock(FabricBlockSettings.create().mapColor(MapColor.TERRACOTTA_GRAY).strength(3.0f, 6.0f).sounds(BlockSoundGroup.COPPER).nonOpaque().blockVision(ModBlocks::never).allowsSpawning(ModBlocks::never).pistonBehavior(PistonBehavior.DESTROY).requiresTool()));
         static { RENDER_LAYER_CUTOUT.add(ANTENNA); }
     public static final Block RADIO = registerBlock("radio", new RadioBlock(FabricBlockSettings.create().mapColor(MapColor.TERRACOTTA_GRAY).strength(1.0f).sounds(BlockSoundGroup.METAL).nonOpaque().blockVision(ModBlocks::never).allowsSpawning(ModBlocks::never).pistonBehavior(PistonBehavior.DESTROY).requiresTool()));
+        static { RENDER_LAYER_CUTOUT.add(RADIO); }
     //</editor-fold>
 
     //<editor-fold desc ="Blocks - Doors">
