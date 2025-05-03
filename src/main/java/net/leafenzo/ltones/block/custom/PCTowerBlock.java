6d0224b865bb3416394f8afbe8ebcb85b9a372fd
amber-meow <maplebernosky@gmail.com>
2025-05-02 22:20:50 -0400

    texture tweaks

---
+++ src/main/java/net/leafenzo/ltones/block/custom/PCTowerBlock.java
@@ -0,0 +1,8 @@
+package net.leafenzo.ltones.block.custom;
+
+public class PCTowerBlock extends BasicHorizontalFacingBlock {
+
+    public PCTowerBlock(Settings settings) {
+        super(settings);
+    }
+}
